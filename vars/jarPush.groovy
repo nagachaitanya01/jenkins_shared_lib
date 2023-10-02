69be41d79b0a5824b5335719308b0c7c088cd0bb
nagachaitanya01 <56996826+nagachaitanya01@users.noreply.github.com>
2023-10-02 17:09:51 +0530

    Update jarPush.groovy

--- vars/jarPush.groovy
+++ vars/jarPush.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh 'curl -X PUT -u admin:Bnc@1234 -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://54.236.8.228:8082/artifactory/example-repo-local/'
+    sh 'curl -X PUT -u admin:Bnc@1234 -T /var/lib/jenkins/workspace/abc/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://54.236.8.228:8082/artifactory/example-repo-local/'
 }
