420501e33457e4f531bc2dbbee15d89d9720c089
nagachaitanya01 <56996826+nagachaitanya01@users.noreply.github.com>
2023-10-02 17:03:04 +0530

    Update jarPush.groovy

--- vars/jarPush.groovy
+++ vars/jarPush.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh './jfrog.py'
+    sh 'curl -X PUT -u admin:Bnc@1234 -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://54.236.8.228:8082/artifactory/example-repo-local/'
 }
